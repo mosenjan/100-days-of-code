1ddc2e1264b56dc7b77654938293573422f340bd
mosenjan <as.mohsen2@gmail.com>
2021-01-13 00:34:51 +0330

    start udemy bache khoshgel course

---
+++ FirstProject/app/src/main/java/com/example/firstproject/Test.kt
@@ -0,0 +1,20 @@
+package com.example.firstproject
+
+fun main(){
+    var courseName: String = "Android MasterClass"
+    val pi:Float = 13.37F
+    var age:Short = 25
+    var year:Short = 2020
+    var randomNumber: Long = 121_212_324_587
+    var isKuni:Boolean = true
+    var firstLetterOfName:Char = 'm'
+
+    println("Hello Word")
+    println(courseName)
+    println(pi)
+    println(age)
+    println(year)
+    println(randomNumber)
+    println(isKuni)
+    println(firstLetterOfName)
+}
