84207e4d520d28800abf87b244c87807ca8d20b4
mosenjan <as.mohsen2@gmail.com>
2021-09-19 17:29:39 +0430

    continue 7minuteWorkout

--- 7MinuteWorkout/app/src/main/java/com/example/a7minuteworkout/ExerciseActivity.kt
+++ 7MinuteWorkout/app/src/main/java/com/example/a7minuteworkout/ExerciseActivity.kt
@@ -1,13 +1,20 @@
 package com.example.a7minuteworkout
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.speech.tts.TextToSpeech
+import android.util.Log
 import android.view.View
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_exercise.*
+import java.util.*
+import kotlin.Exception
+import kotlin.collections.ArrayList
 
-class ExerciseActivity : AppCompatActivity() {
+class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
 
     private var restTimer: CountDownTimer? = null
     private var restProgress = 0
@@ -19,6 +26,10 @@ class ExerciseActivity : AppCompatActivity() {
     private var exerciseList: ArrayList<ExerciseModel>? = null
     private var currentExercisePosition = -1
 
+    private var tts: TextToSpeech? = null
+    private var player: MediaPlayer? = null
+
+    private var exerciseAdapter : ExerciseStatusAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,9 +44,14 @@ class ExerciseActivity : AppCompatActivity() {
             onBackPressed()
         }
 
+
+        tts = TextToSpeech(this,this)
+        exerciseList = Constants.defaultExerciseList()
+
         setupRestView()
 
-        exerciseList = Constants.defaultExerciseList()
+        setupExerciseStatusRecyclerView()
+
 
     }
 
@@ -44,6 +60,21 @@ class ExerciseActivity : AppCompatActivity() {
             restTimer!!.cancel()
             restProgress = 0
         }
+        if (exerciseTimer != null){
+            exerciseTimer!!.cancel()
+            exerciseProgress = 0
+        }
+
+
+        if (tts != null){
+            tts!!.stop()
+            tts!!.shutdown()
+        }
+
+        if (player != null){
+            player!!.stop()
+        }
+
 
         super.onDestroy()
 
@@ -60,12 +91,14 @@ class ExerciseActivity : AppCompatActivity() {
             }
 
             override fun onFinish() {
-
                 currentExercisePosition++
-                Toast.makeText(this@ExerciseActivity,"حالا تمرینو شروع کن",
-                        Toast.LENGTH_SHORT).show()
+
+                exerciseList!![currentExercisePosition].setIsSelected(true)
+                exerciseAdapter!!.notifyDataSetChanged()
+
                 setupExerciseView()
             }
+
         }.start()
 
 
@@ -81,8 +114,16 @@ class ExerciseActivity : AppCompatActivity() {
             }
 
             override fun onFinish() {
-                Toast.makeText(this@ExerciseActivity,"حالا تمرینو تمام کن",
-                        Toast.LENGTH_SHORT).show()
+
+                if (currentExercisePosition < exerciseList?.size!! - 1){
+                    exerciseList!![currentExercisePosition].setIsSelected(false)
+                    exerciseList!![currentExercisePosition].setIsCompleted(true)
+                    exerciseAdapter!!.notifyDataSetChanged()
+                    setupRestView()
+                }else{
+                    Toast.makeText(this@ExerciseActivity,"تبریک، شما تمرین را تمام کردید.",
+                            Toast.LENGTH_SHORT).show()
+                }
             }
         }.start()
 
@@ -90,12 +131,25 @@ class ExerciseActivity : AppCompatActivity() {
     }
 
     private fun setupRestView(){
+
+        llRestView.visibility = View.VISIBLE
+        llExerciseView.visibility = View.GONE
+
+        try{
+            player = MediaPlayer.create(applicationContext, R.raw.press_start)
+            player!!.isLooping = false
+            player!!.start()
+        }catch (e: Exception){
+            e.printStackTrace()
+        }
         if(restTimer != null){
             restTimer!!.cancel()
             restProgress = 0
         }
 
         setRestProgressBar()
+
+        tvNextExerciseName.text = exerciseList!![currentExercisePosition + 1].getName()
     }
     private fun setupExerciseView(){
 
@@ -104,12 +158,47 @@ class ExerciseActivity : AppCompatActivity() {
 
 
         if(exerciseTimer != null){
+
             exerciseTimer!!.cancel()
             exerciseProgress = 0
+
         }
 
+        speakOut(exerciseList!![currentExercisePosition].getName())
+
         setExerciseProgressBar()
+
+        ivImage.setImageResource(exerciseList!![currentExercisePosition].getImage())
+        tvExerciseName.text = exerciseList!![currentExercisePosition].getName()
+
+
+
+
     }
 
+    override fun onInit(status: Int) {
+
+        if (status == TextToSpeech.SUCCESS){
+            val result = tts!!.setLanguage(Locale.ENGLISH)
+            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED)
+                Log.e("TTS","The Language is not supported!")
+        }else{
+            Log.e("TTS","Initialization Failed!")
+        }
+
+    }
+
+
+    private fun speakOut(text: String){
+        tts!!.speak(text, TextToSpeech.QUEUE_FLUSH, null, "")
+
+    }
+
+    private fun setupExerciseStatusRecyclerView(){
+        rvExerciseStatus.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        exerciseAdapter = ExerciseStatusAdapter(exerciseList!!,this)
+        rvExerciseStatus.adapter = exerciseAdapter
+
+    }
 
 }
